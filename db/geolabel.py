c73a8fbb3242990a8c4193ad8ff9e9e93c8062e7
ayushnag <35325113+ayushnag@users.noreply.github.com>
2022-08-08 09:41:04 -0700

    Fixed small errors in geolabel.py

--- db/geolabel.py
+++ db/geolabel.py
@@ -5,8 +5,8 @@ import numpy as np
 import pandas as pd
 import scipy.io as sio
 import matplotlib.pyplot as plt
-import cartopy
-import cartopy.crs as ccrs
+# import cartopy
+# import cartopy.crs as ccrs
 import geopandas as gpd
 from shapely.geometry import Polygon
 
@@ -24,7 +24,7 @@ class GeoLabel:
     def create_fronts_zones_shapes():
         """Creates shapefiles for fronts and zones between fronts"""
         # Writes the Kim & Orsi 2014 fronts to a shapefile
-        mat_data: dict = sio.loadmat(GeoLabel.kim_orsi_file)
+        mat_data: dict = sio.loadmat('../data/fronts/ys_fronts.mat')
         # fronts = ([[lat1, lon1], [lat2, lon2]], [[lat1, lon1], ...], ...)
         orsi_data: tuple = mat_data['ys_fronts'].tolist()[0][0]
         # filters out extra data and only keeps the 4 Southern Ocean fronts
@@ -32,31 +32,35 @@ class GeoLabel:
         # points from each front that follow a smooth contour (no holes in the front)
         keep = ((44, 2633), (0, 2305), (0, 2614), (60, 2154))
         front_names: tuple = ('SAF', 'PF', 'SACC', 'SBdy')
-        fronts: dict = {}
+        shapes: dict = {}
         for i, orsi_front in enumerate(orsi_data):
             # convert np array to df
             front_df = pd.DataFrame(data=orsi_front, columns=['Latitude', 'Longitude'], 
-                                                     index=np.arange(len(orsi_front)))
+                                                        index=np.arange(len(orsi_front)))
             # remove extra points such as small holes in the front
             front_df = front_df.iloc[keep[i][0]:keep[i][1]]
             # data has points [-180, 360] but [-180, 180] is duplicate of [0, 360]
             front_df = front_df[front_df.Latitude <= 180]
             # make new polygon and add to list
             name = front_names[i]
-            fronts[name] = Polygon(zip(front_df.Latitude, front_df.Longitude))
+            shapes[name] = (Polygon(zip(front_df.Latitude, front_df.Longitude)))
 
-        fronts_gdf = gpd.GeoDataFrame(fronts, crs='EPSG:3031')
-        fronts_gdf.to_file(GeoLabel.fronts_shapefile)
+        fronts_gdf = gpd.GeoDataFrame({'front': front_names, 'geometry': shapes.values()}, crs='EPSG:4326')
+        fronts_gdf = fronts_gdf.to_crs(crs='EPSG:3031')
+        # fronts_gdf = fronts_gdf.set
+        # fronts_gdf.to_file('../data/shapefiles/fronts/so_fronts.shp')
 
-        world: gpd.GeoDataFrame = gpd.read_file(GeoLabel.world_shapefile)
+
+        world: gpd.GeoDataFrame = gpd.read_file('../data/shapefiles/world/world.shp')
         antarctica: gpd.GeoDataFrame = world[world['NAME'] == 'Antarctica']
         antarctica = antarctica.to_crs(epsg=3031)
         antarctica: Polygon = antarctica['geometry'].values[0]
 
-        zones: dict = {'zone': ['PFZ', 'ASZ', 'SOZ', 'SIZ'], 'geometry': [fronts['SAF'] - fronts['PF'], 
-                                                                        fronts['PF'] - fronts['SACC'], 
-                                                                        fronts['SACC'] - fronts['SBdy'],
-                                                                        fronts['SIE'] - antarctica]}
+        shapes = dict(zip(fronts_gdf['front'], fronts_gdf['geometry']))
+        zones: dict = {'zone': ['PFZ', 'ASZ', 'SOZ', 'SIZ'], 'geometry': [shapes['SAF'] - shapes['PF'], 
+                                                                        shapes['PF'] - shapes['SACC'], 
+                                                                        shapes['SACC'] - shapes['SBdy'],
+                                                                        shapes['SBdy'] - antarctica]}
         zones_gdf = gpd.GeoDataFrame(zones, crs='EPSG:3031')
         zones_gdf.to_file(GeoLabel.zones_shapefile)
 
@@ -92,32 +96,32 @@ class GeoLabel:
         """Labels provided data with sectors"""
         print('temp')
 
-    @staticmethod
-    def plot_orsi_fronts():
-        """Plots the contents of ys_fronts.mat on a Southern Ocean map"""
-        map_proj = ccrs.SouthPolarStereo()
-        fig = plt.figure(figsize=[20, 20])  # inches
-        ax = plt.subplot(projection=map_proj)
-        ax.set_extent([-180, 180, -90, -39.4], ccrs.PlateCarree())
-        fig.subplots_adjust(bottom=0.05, top=0.95, left=0.04, right=0.95, wspace=0.02)
+    # @staticmethod
+    # def plot_orsi_fronts():
+    #     """Plots the contents of ys_fronts.mat on a Southern Ocean map"""
+    #     map_proj = ccrs.SouthPolarStereo()
+    #     fig = plt.figure(figsize=[20, 20])  # inches
+    #     ax = plt.subplot(projection=map_proj)
+    #     ax.set_extent([-180, 180, -90, -39.4], ccrs.PlateCarree())
+    #     fig.subplots_adjust(bottom=0.05, top=0.95, left=0.04, right=0.95, wspace=0.02)
 
-        ax.add_feature(cartopy.feature.LAND)
-        ax.add_feature(cartopy.feature.COASTLINE)
-        ax.gridlines(draw_labels=True)
+    #     ax.add_feature(cartopy.feature.LAND)
+    #     ax.add_feature(cartopy.feature.COASTLINE)
+    #     ax.gridlines(draw_labels=True)
 
-        mat_data: dict = sio.loadmat(GeoLabel.kim_orsi_file)
-        # mat_data = ([[lat1, lon1], [lat2, lon2]], [[lat1, lon1], ...], ...)
-        orsi_fronts: tuple = mat_data['ys_fronts'].tolist()[0][0]
-        # filters out extra data and only keeps the 4 Southern Ocean fronts
-        orsi_fronts = orsi_fronts[:4]
-        colors = ['c', 'b', 'g', 'r']
-        for i, orsi_front in enumerate(orsi_fronts):
-            front = orsi_front.T
-            # data has points [-180, 360] but [-180, 180] is duplicate of [0, 360]
-            extra = np.where(front[0] > 180)
-            # delete [180, 360]
-            lat, lon = np.delete(front[0], extra), np.delete(front[1], extra)
-            # remove NaN's
-            lat, lon = lat[~np.isnan(lat)], lon[~np.isnan(lon)]
-            ax.plot(lat, lon, marker='o', linestyle='', color=colors[i], transform=ccrs.PlateCarree())
-        plt.show()
+    #     mat_data: dict = sio.loadmat(GeoLabel.kim_orsi_file)
+    #     # mat_data = ([[lat1, lon1], [lat2, lon2]], [[lat1, lon1], ...], ...)
+    #     orsi_fronts: tuple = mat_data['ys_fronts'].tolist()[0][0]
+    #     # filters out extra data and only keeps the 4 Southern Ocean fronts
+    #     orsi_fronts = orsi_fronts[:4]
+    #     colors = ['c', 'b', 'g', 'r']
+    #     for i, orsi_front in enumerate(orsi_fronts):
+    #         front = orsi_front.T
+    #         # data has points [-180, 360] but [-180, 180] is duplicate of [0, 360]
+    #         extra = np.where(front[0] > 180)
+    #         # delete [180, 360]
+    #         lat, lon = np.delete(front[0], extra), np.delete(front[1], extra)
+    #         # remove NaN's
+    #         lat, lon = lat[~np.isnan(lat)], lon[~np.isnan(lon)]
+    #         ax.plot(lat, lon, marker='o', linestyle='', color=colors[i], transform=ccrs.PlateCarree())
+    #     plt.show()
