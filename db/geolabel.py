1d08bc129189015b48e718b57a4c9f7c7c8cdcd4
ayushnag <35325113+ayushnag@users.noreply.github.com>
2022-08-15 11:21:42 -0700

    Updated geolabel.py to use the NSIDC SMMR v3 for sea ice concentration

--- db/geolabel.py
+++ db/geolabel.py
@@ -4,8 +4,14 @@ from os.path import exists
 import numpy as np
 import pandas as pd
 import scipy.io as sio
+import cartopy
+import cartopy.crs as ccrs
+import matplotlib as plt
 import geopandas as gpd
+import alphashape
+import pyproj
 from shapely.geometry import Polygon
+from shapely.ops import transform
 
 
 class GeoLabel:
@@ -25,11 +31,12 @@ class GeoLabel:
         # fronts = ([[lat1, lon1], [lat2, lon2]], [[lat1, lon1], ...], ...)
         orsi_data: tuple = mat_data['ys_fronts'].tolist()[0][0]
         # filters out extra data and only keeps the 4 Southern Ocean fronts
-        orsi_data = orsi_data[:4]
+        orsi_data = orsi_data[:3]
         # points from each front that follow a smooth contour (no holes in the front)
-        keep = ((44, 2633), (0, 2305), (0, 2614), (60, 2154))
-        front_names: tuple = ('SAF', 'PF', 'SACC', 'SBdy')
+        keep = ((44, 2633), (0, 2305), (0, 2614))
+        front_names: tuple = ('SAF', 'PF', 'SACC', 'SIE')
         shapes: dict = {}
+        project = pyproj.Transformer.from_crs(pyproj.CRS('EPSG:4326'), pyproj.CRS('EPSG:3031'), always_xy=True).transform
         for i, orsi_front in enumerate(orsi_data):
             # convert np array to df
             front_df = pd.DataFrame(data=orsi_front, columns=['Latitude', 'Longitude'],
@@ -40,29 +47,40 @@ class GeoLabel:
             front_df = front_df[front_df.Latitude <= 180]
             # make new polygon and add to list
             name = front_names[i]
-            shapes[name] = (Polygon(zip(front_df.Latitude, front_df.Longitude)))
+            shapes[name] = transform(project, Polygon(zip(front_df.Latitude, front_df.Longitude)))
 
-        fronts_gdf = gpd.GeoDataFrame({'front': front_names, 'geometry': shapes.values()}, crs='EPSG:4326')
-        fronts_gdf = fronts_gdf.to_crs(crs='EPSG:3031')
-        # fronts_gdf = fronts_gdf.set
-        # fronts_gdf.to_file('../data/shapefiles/fronts/so_fronts.shp')
+        f = open(r"../data/sea_ice/mean.sep.1979-2021.s", "r")
+        ice = np.fromfile(f, dtype=np.uint8)
+        edge = np.logical_and(15 <= ice, ice <= 25)
+        dx = dy = 25000
+        x = np.arange(-3950000, +3950000, +dx)
+        y = np.arange(+4350000, -3950000, -dy)
+        grid = np.dstack(np.meshgrid(x, y)).reshape(-1, 2)
+        points = grid[edge].T
+        gdf = gpd.GeoDataFrame(geometry=gpd.points_from_xy(x=points[0], y=points[1]), crs='EPSG:3031')
+        # Generating alpha shape takes ~8 minutes
+        alpha: gpd.GeoDataFrame = alphashape.alphashape(gdf)
+        sie: Polygon = alpha['geometry'].values[0]
+        shapes['SIE'] = sie
 
-        world: gpd.GeoDataFrame = gpd.read_file('../data/shapefiles/world/world.shp')
+        fronts_gdf = gpd.GeoDataFrame({'front': shapes.keys(), 'geometry': shapes.values()}, crs='EPSG:3031')
+        fronts_gdf.to_file(GeoLabel.fronts_shapefile)
+
+        world: gpd.GeoDataFrame = gpd.read_file(GeoLabel.world_shapefile)
         antarctica: gpd.GeoDataFrame = world[world['NAME'] == 'Antarctica']
         antarctica = antarctica.to_crs(epsg=3031)
         antarctica: Polygon = antarctica['geometry'].values[0]
 
-        shapes = dict(zip(fronts_gdf['front'], fronts_gdf['geometry']))
         zones: dict = {'zone': ['PFZ', 'ASZ', 'SOZ', 'SIZ'], 'geometry': [shapes['SAF'] - shapes['PF'],
                                                                           shapes['PF'] - shapes['SACC'],
-                                                                          shapes['SACC'] - shapes['SBdy'],
-                                                                          shapes['SBdy'] - antarctica]}
+                                                                          shapes['SACC'] - shapes['SIE'],
+                                                                          shapes['SIE'] - antarctica]}
         zones_gdf = gpd.GeoDataFrame(zones, crs='EPSG:3031')
         zones_gdf.to_file(GeoLabel.zones_shapefile)
 
     @staticmethod
     def get_zone(lat: float, lon: float) -> str:
-        # assert that the range is in the Southern Ocean
+        assert lat <= -30, "Provided latitude is not in the Southern Ocean (must be less than -30 degrees)"
         # Calls label_fronts with lat, lon pair as DataFrame
         return GeoLabel.label_zones(pd.DataFrame([(lat, lon)], columns=['lat', 'lon']), 'lat', 'lon')['zone'][0]
 
@@ -84,40 +102,43 @@ class GeoLabel:
 
     @staticmethod
     def get_sector(lat: float, lon: float) -> str:
-        # assert that the range is in the Southern Ocean
-        print('temp')
+        assert lat <= -30, "Provided latitude is not in the Southern Ocean (must be less than -30 degrees)"
+        # Calls label_fronts with lat, lon pair as DataFrame
+        return GeoLabel.label_sectors(pd.DataFrame([(lat, lon)], columns=['lat', 'lon']), 'lat', 'lon')['sector'][0]
 
     @staticmethod
     def label_sectors(data: pd.DataFrame, lat_col: str, lon_col: str) -> pd.DataFrame:
         """Labels provided data with sectors"""
+        # So the way Im going to do this is with lines of longitude
+        # so if it's x then do blah
         print('temp')
 
-    # @staticmethod
-    # def plot_orsi_fronts():
-    #     """Plots the contents of ys_fronts.mat on a Southern Ocean map"""
-    #     map_proj = ccrs.SouthPolarStereo()
-    #     fig = plt.figure(figsize=[20, 20])  # inches
-    #     ax = plt.subplot(projection=map_proj)
-    #     ax.set_extent([-180, 180, -90, -39.4], ccrs.PlateCarree())
-    #     fig.subplots_adjust(bottom=0.05, top=0.95, left=0.04, right=0.95, wspace=0.02)
+    @staticmethod
+    def plot_orsi_fronts():
+        """Plots the contents of ys_fronts.mat on a Southern Ocean map"""
+        map_proj = ccrs.SouthPolarStereo()
+        fig = plt.figure(figsize=[20, 20])  # inches
+        ax = plt.subplot(projection=map_proj)
+        ax.set_extent([-180, 180, -90, -39.4], ccrs.PlateCarree())
+        fig.subplots_adjust(bottom=0.05, top=0.95, left=0.04, right=0.95, wspace=0.02)
 
-    #     ax.add_feature(cartopy.feature.LAND)
-    #     ax.add_feature(cartopy.feature.COASTLINE)
-    #     ax.gridlines(draw_labels=True)
+        ax.add_feature(cartopy.feature.LAND)
+        ax.add_feature(cartopy.feature.COASTLINE)
+        ax.gridlines(draw_labels=True)
 
-    #     mat_data: dict = sio.loadmat(GeoLabel.kim_orsi_file)
-    #     # mat_data = ([[lat1, lon1], [lat2, lon2]], [[lat1, lon1], ...], ...)
-    #     orsi_fronts: tuple = mat_data['ys_fronts'].tolist()[0][0]
-    #     # filters out extra data and only keeps the 4 Southern Ocean fronts
-    #     orsi_fronts = orsi_fronts[:4]
-    #     colors = ['c', 'b', 'g', 'r']
-    #     for i, orsi_front in enumerate(orsi_fronts):
-    #         front = orsi_front.T
-    #         # data has points [-180, 360] but [-180, 180] is duplicate of [0, 360]
-    #         extra = np.where(front[0] > 180)
-    #         # delete [180, 360]
-    #         lat, lon = np.delete(front[0], extra), np.delete(front[1], extra)
-    #         # remove NaN's
-    #         lat, lon = lat[~np.isnan(lat)], lon[~np.isnan(lon)]
-    #         ax.plot(lat, lon, marker='o', linestyle='', color=colors[i], transform=ccrs.PlateCarree())
-    #     plt.show()
+        mat_data: dict = sio.loadmat(GeoLabel.kim_orsi_file)
+        # mat_data = ([[lat1, lon1], [lat2, lon2]], [[lat1, lon1], ...], ...)
+        orsi_fronts: tuple = mat_data['ys_fronts'].tolist()[0][0]
+        # filters out extra data and only keeps the 4 Southern Ocean fronts
+        orsi_fronts = orsi_fronts[:4]
+        colors = ['c', 'b', 'g', 'r']
+        for i, orsi_front in enumerate(orsi_fronts):
+            front = orsi_front.T
+            # data has points [-180, 360] but [-180, 180] is duplicate of [0, 360]
+            extra = np.where(front[0] > 180)
+            # delete [180, 360]
+            lat, lon = np.delete(front[0], extra), np.delete(front[1], extra)
+            # remove NaN's
+            lat, lon = lat[~np.isnan(lat)], lon[~np.isnan(lon)]
+            ax.plot(lat, lon, marker='o', linestyle='', color=colors[i], transform=ccrs.PlateCarree())
+        plt.show()
