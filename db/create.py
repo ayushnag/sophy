135bbd9c12ef0be65efc60cd8618ccb199b15691
ayushnag <35325113+ayushnag@users.noreply.github.com>
2022-09-27 11:19:23 -0700

    Created new helper class sophysql.py and shifted functionality from create.py

--- db/create.py
+++ db/create.py
@@ -5,27 +5,9 @@ import warnings
 import sqlite3
 import pandas as pd
 import pyworms
+import geolabel
+import sophysql
 from pandas import DataFrame
-from geolabel import GeoLabel
-
-# columns expected in the sample table
-sample_cols: tuple = ("latitude", "longitude", "timestamp", "front_zone", "sector", "depth", "pressure",
-                      "tot_depth_water_col", "source_name", "aphia_id", "salinity", "temperature", "density",
-                      "chlorophyll", "phaeopigments", "fluorescence", "primary_prod", "cruise", "down_par",
-                      "light_intensity", "light_transmission", "mld", "scientific_name", "prasinophytes",
-                      "cryptophytes", "mixed_flagellates", "diatoms", "haptophytes", "nitrate", "nitrite", "pco2",
-                      "diss_oxygen", "diss_inorg_carbon", "diss_inorg_nitrogen", "diss_inorg_phosp", "diss_org_carbon",
-                      "diss_org_nitrogen", "part_org_carbon", "part_org_nitrogen", "org_carbon", "org_matter",
-                      "org_nitrogen", "phosphate", "silicate", "tot_nitrogen", "tot_part_carbon", "tot_phosp", "ph",
-                      "origin_id", "strain", "notes")
-
-# columns expected in the occurence table
-occurrence_cols: tuple = ("latitude", "longitude", "timestamp", "aphia_id", "front_zone", "sector", "depth", "notes")
-
-# columns expected in the taxonomy table
-taxa_cols: tuple = ("aphia_id", "scientific_name", "superkingdom", "kingdom", "phylum", "subphylum", "superclass",
-                    "class", "subclass", "superorder", "t_order", "suborder", "infraorder", "superfamily",
-                    "family", "genus", "species", "modified")
 
 # worms output -> sql col name. Also include columns from the original data that are needed but don't need renaming
 # Ex: "class" -> "class" means col name is correct but class column is needed for calculations and/or used in database
@@ -35,50 +17,40 @@ worms_sql: dict = {"AphiaID": "aphia_id", "scientificname": "scientific_name", "
                    "suborder": "suborder", "infraorder": "infraorder", "superfamily": "superfamily",
                    "family": "family", "genus": "genus", "species": "species", "modified": "modified"}
 
-# lter -> sql col name. Also include columns from the original data that are needed but don't need renaming
-lter_sql: dict = {"DatetimeGMT": "timestamp", "Latitude": "latitude", "Longitude": "longitude",
-                  "Depth": "depth", "Temperature": "temperature", "Salinity": "salinity", "Density": "density",
-                  "Chlorophyll": "chlorophyll", "Fluorescence": "fluorescence", "Phaeopigment": "phaeopigments",
-                  "PrimaryProduction": "primary_prod", "studyName": "cruise", "PAR": "down_par",
-                  "Prasinophytes": "prasinophytes", "Cryptophytes": "cryptophytes",
-                  "MixedFlagellates": "mixed_flagellates", "Diatoms": "diatoms", "Haptophytes": "haptophytes",
-                  "NO3": "nitrate", "NO2": "nitrite", "DIC1": "diss_inorg_carbon", "DOC": "diss_org_carbon",
-                  "POC": "part_org_carbon", "SiO4": "silicate", "N": "tot_nitrogen",
-                  "PO4": "phosphate", "Notes1": "notes"}
+con = sqlite3.connect("sophy.db")
+cur = con.cursor()
+sophysql.create_tables()
 
-# phytobase -> sql col name. Also include columns from the original data that are needed but don't need renaming
-phybase_sql: dict = {"scientificName": "scientific_name", "decimalLongitude": "longitude",
-                     "decimalLatitude": "latitude", "year": "year", "month": "month", "day": "day",
-                     "depth": "depth"}
+sample_cols: tuple = sophysql.get_table_cols("sample")
+occurrence_cols: tuple = sophysql.get_table_cols("occurrence")
+taxa_cols: tuple = sophysql.get_table_cols("taxonomy")
 
 
 def write_lter() -> None:
     """Writes LTER dataset to database"""
     sample_df: DataFrame = pd.read_csv('../data/datasets/lter.csv', encoding='unicode_escape')
-    sample_df = clean_df(sample_df, lter_sql)
 
     # drop rows with null time, lat, or long
     sample_df = sample_df.dropna(subset=['timestamp', 'longitude', 'latitude'])
 
-    sample_df = GeoLabel.label_zones(sample_df, 'longitude', 'latitude').drop('index_right', axis=1)
-    sample_df = GeoLabel.label_sectors(sample_df, 'longitude')
+    sample_df = geolabel.label_zones(sample_df, 'longitude', 'latitude').drop('index_right', axis=1)
+    sample_df = geolabel.label_sectors(sample_df, 'longitude')
 
     # write sample dataframe to sql database
-    write_df_sql("sample", sample_df, sample_cols)
+    sophysql.write_dataset("sample", sample_df)
 
 
 def write_phybase() -> None:
     """Writes Phytobase dataset to database"""
     occ_df: DataFrame = pd.read_csv('../data/datasets/phytobase.csv', encoding='unicode_escape')
-    occ_df = clean_df(occ_df, phybase_sql)
 
     sci_names_data = set(occ_df['scientific_name'].unique())
     sci_names_micro = set(cur.execute("select scientific_name from taxonomy").fetchall())
     missing: set = sci_names_data - sci_names_micro  # sci_names that are missing from our database taxa records
     # get full taxonomy of microscopy data as dataframe
     # TODO: enable this line when not testing; taxa_df: DataFrame = worms_taxa(list(missing))
-    taxa_df: DataFrame = clean_df(pd.read_csv('../data/datasets/micro_phybase.csv'), worms_sql)  # Testing only
-
+    taxa_df: DataFrame = pd.read_csv('../data/datasets/sample_worms.csv')  # Testing only
+    taxa_df = taxa_df.rename(columns=worms_sql)
     # keep only data in the Southern Ocean (latitude < -30 degrees)
     # not filtered earlier to get maximum taxa data possible for microscopy table
     occ_df = occ_df[occ_df['latitude'] <= -30]
@@ -91,24 +63,13 @@ def write_phybase() -> None:
     # join on sample and taxonomy (by aphia_id), only keep cols in the occurrence table (filter out order, genus, etc)
     occ_df: DataFrame = pd.merge(occ_df, taxa_df).filter(occurrence_cols)
 
-    occ_df = GeoLabel.label_zones(occ_df, 'longitude', 'latitude').drop('index_right', axis=1)
-    occ_df = GeoLabel.label_sectors(occ_df, 'longitude')
+    occ_df = geolabel.label_zones(occ_df, 'longitude', 'latitude').drop('index_right', axis=1)
+    occ_df = geolabel.label_sectors(occ_df, 'longitude')
 
     # write taxonomy dataframe to sql database
-    write_df_sql("taxonomy", taxa_df, taxa_cols)
+    sophysql.write_dataset(table_name="taxonomy", data=taxa_df)
     # write occurrence dataframe to sql database
-    write_df_sql("occurrence", occ_df, occurrence_cols)
-
-
-def write_df_sql(table_name: str, data: DataFrame, cols: tuple) -> None:
-    """Writes data frame to the SQLite table table_name:param"""
-    assert set(data.columns.values.tolist()).issubset(set(cols)), f'Provided {table_name} table has invalid column(s)'
-    # Create temp table, send data to table_name, and drop temp table
-    data.to_sql("temp", con=con, index=False)
-    cols_str: str = csl(data.columns.values.tolist())
-    cur.execute(f"insert into {table_name} ({cols_str}) select {cols_str} from temp")
-    cur.execute(f"drop table temp")
-    con.commit()
+    sophysql.write_dataset(table_name="occurrence", data=occ_df)
 
 
 def worms_taxa(taxa: list) -> DataFrame:
@@ -124,26 +85,20 @@ def worms_taxa(taxa: list) -> DataFrame:
     if len(no_result) != 0:  # outputs taxa (first 20) where WoRMS database found no result
         warnings.warn(f"No results found by WoRMS database: {str(no_result[:20])}...")
     # convert list of dict() -> dataframe and clean it
-    return clean_df(pd.DataFrame(microscopy), worms_sql)
+    return DataFrame(microscopy).rename(worms_sql)
 
 
-def clean_df(data: DataFrame, df_sql_map: dict) -> DataFrame:
-    """Performs few operations on DF for ease of use and prepare for inserting into SQLite table"""
-    data = data.filter(df_sql_map.keys())  # filter out columns that are not in the set
-    return data.rename(columns=df_sql_map)  # rename columns using dict()
+def basic_fixes(data: DataFrame) -> DataFrame:
+    """Performs basic fixes on DF before inserting into SQLite table"""
+    data = data[data['latitude'] <= -30]
+    data = data.dropna(subset=['longitude', 'latitude'])
+    return data
 
 
 def csl(cols: list) -> str:
     """Converts list of values to comma separated string. Ex: [A, B, C] --> A, B, C"""
     return ', '.join(cols)
 
-
-con = sqlite3.connect("sophy.db")
-cur = con.cursor()
-
-with open('create_tables.sql', 'r') as create_tables:
-    con.executescript(create_tables.read())
-con.commit()
 
 write_lter()
 write_phybase()
---
+++ db/sophysql.py
@@ -0,0 +1,59 @@
+"""Manages interactions between data and SQL database"""
+__author__ = 'Ayush Nag'
+
+import sqlite3
+
+from pandas import DataFrame
+
+con = sqlite3.connect("sophy.db")
+cur = con.cursor()
+
+
+def create_tables() -> None:
+    """Creates tables based on schema and fields in lists"""
+    with open('create_tables.sql', 'r') as sql_file:
+        con.executescript(sql_file.read())
+    con.commit()
+
+
+def get_table_cols(table: str) -> tuple:
+    """TODO: update docstring"""
+    data = cur.execute(f"pragma table_info({table})").fetchall()
+    if data:
+        return list(zip(*data))[1]
+    else:
+        raise ValueError(f"Table {table} does not exist in the database")
+
+
+def write_dataset(table_name: str, data: DataFrame) -> None:
+    """Writes data frame to the SQLite table table_name:param"""
+    # Columns present in data and in the destination SQL table
+    common = set(data.columns.values.tolist()).intersection(set(get_table_cols(table_name)))
+    assert len(common) != 0, "No columns in provided data match given table"
+    # Create temp table, send data to table_name, and drop temp table
+    data.to_sql("temp", con=con, index=False)
+    cols_str: str = csl(list(common))
+    cur.execute(f"insert into {table_name} ({cols_str}) select {cols_str} from temp")
+    cur.execute(f"drop table temp")
+    con.commit()
+
+
+def query(query: str):
+    # TODO: do some checks/give useful error messages for query
+    # TODO: format output to return DataFrame instead of list
+    return cur.execute(query).fetchall()
+
+
+def full() -> DataFrame:
+    """TODO: Returns the entire SOPhy database in a single DataFrame"""
+
+
+def clean_df(data: DataFrame, df_sql_map: dict) -> DataFrame:
+    """Performs few operations on DF for ease of use and prepare for inserting into SQLite table"""
+    data = data.filter(df_sql_map.keys())  # filter out columns that are not in the set
+    return data.rename(columns=df_sql_map)  # rename columns using dict()
+
+
+def csl(cols: list) -> str:
+    """Converts list of values to comma separated string. Ex: [A, B, C] --> A, B, C"""
+    return ', '.join(cols)
