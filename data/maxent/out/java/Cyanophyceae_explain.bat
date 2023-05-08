2471b5cfc081aceb669d91533ca02bc60165711f
ayushnag <35325113+ayushnag@users.noreply.github.com>
2023-05-08 02:44:36 -0700

    clean up directory

--- data/maxent/out/java/Cyanophyceae_explain.bat
+++
@@ -1,2 +0,0 @@
-java -mx500m -cp "/C:/Users/Ayush Nag/Downloads/maxent/maxent/maxent.jar;maxent.jar" density.Explain -l "C:\Users\Ayush Nag\vault\sophy\data\out\max_ent_out\Cyanophyceae.lambdas" -c "C:\Users\Ayush Nag\vault\sophy\data\out\max_ent_out\Cyanophyceae.asc" "C:\Users\Ayush Nag\vault\sophy\data\out\cnn_input_layers\max_ent_layers_in"
-@if errorlevel 1 pause
